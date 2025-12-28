569f3bdb154256a54578cfb5a677a51039289a09
pipeargill <matthew.g.beaver@gmail.com>
2025-12-28 02:40:45 -0600

    vault backup: 2025-12-28 02:40:44

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Pipeargill",
     pageTitleSuffix: "",
     enableSPA: false,
     enablePopovers: true,
